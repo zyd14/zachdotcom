e4f751194fe57c7019604b33925562078f39f47d
zach romer <romerzs14@gmail.com>
2019-11-11 16:59:45 -0800

    Wrangled around enough to get layout.html inheritance working.  Got 404 response page working.

--- mysite/app.py
+++ mysite/app.py
@@ -1,7 +1,7 @@
 import logging
 from logging.config import dictConfig
 
-from flask import Flask, has_request_context, request
+from flask import Flask, has_request_context, request, render_template
 from flask.logging import default_handler
 from flask_bootstrap import Bootstrap
 
@@ -47,6 +47,7 @@ def create_app(config_object='mysite.settings'):
 
     register_exensions(app)
     register_blueprints(app)
+    register_errorhandlers(app)
     return app
 
 def register_blueprints(app):
@@ -54,6 +55,19 @@ def register_blueprints(app):
 
 def register_exensions(app):
     Bootstrap(app)
+
+def register_errorhandlers(app):
+    """Register error handlers."""
+
+    def render_error(error):
+        """Render error template."""
+        # If a HTTPException, pull the `code` attribute; default to 500
+        error_code = getattr(error, "code", 500)
+        return render_template(f"{error_code}.html"), error_code
+
+    for errcode in [401, 404, 500]:
+        app.errorhandler(errcode)(render_error)
+    return None
 
 if __name__ == '__main__':
     app = create_app()
